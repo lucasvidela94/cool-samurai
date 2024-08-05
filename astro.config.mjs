309bddd1f2fcde901caa57a25540c6c981b8dbc0
lucas <lucas.videla994@gmail.com>
2024-08-05 19:10:14 -0300

    feat:added cloudfare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  site: "https://lucasvidela94.github.io",
+  output: "hybrid",
+  site: "https://lucasvidela.dev.ar/",
   base: "",
   content: {
     sources: ["src/content"],
   },
   integrations: [mdx()],
+  adapter: cloudflare(),
 });
